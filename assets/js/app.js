0d05453b6c0c8e5d9ac99f0e19ea0b9fc635379e
Ana Durand <ana.dc1004@gmail.com>
2017-04-24 13:24:44 -0500

    valor por default

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,3 +16,9 @@ function getURL(e){
   }
 }
 inputURL.onkeydown = getURL;
+window.addEventListener("load", function(){
+  var arrayUrl = ["https://i.stack.imgur.com/lj5Pk.jpg","https://imgix.ranker.com/user_node_img/68/1343066/original/kakashi-hatake-comic-book-characters-photo-u6?w=650&q=50&fm=jpg","http://www.10wallpaper.com/wallpaper/medium/1206/shugo_chara-Anime_characters_wallpaper_medium.jpg"];
+  arrayUrl.forEach(function(item){
+    creandoDiv(item);
+  });
+});
